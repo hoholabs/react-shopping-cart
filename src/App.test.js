e018924971f1c9ae541d8ee6c60fac5e455b6854
hoholabs <johnpaulhoran@gmail.com>
2022-10-18 14:07:43 -0400

    added some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom'; // optional
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the page', () => {
+    render(<App />);
+    const title = screen.getByText(/Fake Store/i);
+    expect(title).toBeInTheDocument();
+});
+
+test('clicking shop takes you to the shop', () => {
+    render(<App />);
+    const shopButton = screen.getByRole('link', { name: 'Shop' });
+    userEvent.click(shopButton);
+    const thingus = screen.getByText(/thingus/i);
+    expect(thingus).toBeInTheDocument();
 });
